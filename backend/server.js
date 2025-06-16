a364e1c4b8ebde7ae258ac6ceb2f23415e873356
paramka0 <pamksf6@gmail.com>
2025-06-16 15:37:47 +0700

    Удалён блок отдачи фронта из бэкенда для Render

--- backend/server.js
+++ backend/server.js
@@ -38,13 +38,13 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(cookieParser());
 
 // Serve static files from the React app in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../frontend/build')));
   
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+//   });
+// }
 
 // Создание таблиц при запуске
 createTables();
